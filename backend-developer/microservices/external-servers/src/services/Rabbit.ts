f5b0cf5b4822995c45e37739779638be9d83df87
Thiago Henrique <thiagohp070@gmail.com>
2024-01-22 17:29:00 -0300

    feat: consume and producer from external server

--- backend-developer/microservices/external-servers/src/services/Rabbit.ts
+++ backend-developer/microservices/external-servers/src/services/Rabbit.ts
@@ -1,4 +1,10 @@
 import amqplib from "amqplib";
+import { genDescription } from "../utils/generate";
+import { IResponse } from "../types/response";
+
+interface IRequest {
+  cpf: String;
+}
 
 (async () => {
   try {
@@ -7,20 +13,28 @@ import amqplib from "amqplib";
     const conn = await amqplib.connect("amqp://localhost");
 
     const qUpRequest = await conn.createChannel();
+    const qUpResponse = await conn.createChannel();
     await qUpRequest.assertQueue(upReqName);
 
     qUpRequest.consume(upReqName, (msg) => {
       if (msg) {
-        console.log(msg.content.toString());
+        const parsedMsgContent: IRequest = JSON.parse(msg.content.toString());
+        const description: IResponse = genDescription(parsedMsgContent.cpf);
+
         qUpRequest.ack(msg);
+
+        setTimeout(() => {
+          qUpResponse.sendToQueue(
+            upResName,
+            Buffer.from(JSON.stringify(description))
+          );
+        }, Math.random() * 1000);
       }
     });
 
-    const qUpResponse = await conn.createChannel();
-
-    setInterval(() => {
-      qUpResponse.sendToQueue(upResName, Buffer.from("iaee"));
-    }, 1000);
+    // setInterval(() => {
+    //   qUpResponse.sendToQueue(upResName, Buffer.from("iaee"));
+    // }, 1000);
   } catch (error) {
     if (error instanceof Error) console.log(error.message);
   }
---
+++ backend-developer/microservices/external-servers/src/utils/generate.ts
@@ -0,0 +1,33 @@
+import { IResponse } from "../types/response";
+
+function genDateWithTime(): Date {
+  const salt = Math.trunc(Math.random() * 10);
+  const day = ((salt * 10) % 30) + 1;
+  const month = ((salt * 5) % 12) + 1;
+  const year = salt + 10;
+
+  const fullDate = `${month}-${day}-20${year}`;
+  console.log(fullDate);
+
+  return new Date(fullDate);
+}
+
+export function genDescription(cpf: String): IResponse {
+  const salt = Math.trunc(Math.random() * 10);
+
+  const init = `A pessoa inscrita no cpf: ${cpf} se envolveu em um`;
+  const events = ["acidente", "assalto", "golpe"];
+  const guilt = ["culpado", "vítima", "cumplice", "espectador", "testemunha"];
+
+  const date = genDateWithTime().toISOString();
+
+  const description = `${init} ${events[salt % events.length]} como ${
+    guilt[salt % guilt.length]
+  } em ${date}`;
+
+  return {
+    cpf,
+    date,
+    description,
+  };
+}
