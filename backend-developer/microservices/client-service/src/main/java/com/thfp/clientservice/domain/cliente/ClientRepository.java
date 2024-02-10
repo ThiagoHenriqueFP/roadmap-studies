2b351abf6a2bcc3740732d47352570a8ce662b4a
Thiago Henrique <thiagohp070@gmail.com>
2024-02-09 22:12:42 -0300

    feat: populate client infos list and create aux methods

--- backend-developer/microservices/client-service/src/main/java/com/thfp/clientservice/domain/cliente/ClientRepository.java
+++ backend-developer/microservices/client-service/src/main/java/com/thfp/clientservice/domain/cliente/ClientRepository.java
@@ -1,8 +1,17 @@
 package com.thfp.clientservice.domain.cliente;
 
+import com.thfp.clientservice.domain.cliente_infos.ClientInfos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ClientRepository extends JpaRepository<Client, UUID>  {
+    Optional<Client> findByCpf(String cpf);
+
+    // custom query to resolve lazy fetch problem
+    @Query("SELECT c.clientInfos FROM Client c WHERE c.id = :id")
+    List<ClientInfos> findInfosById(UUID id);
 }
