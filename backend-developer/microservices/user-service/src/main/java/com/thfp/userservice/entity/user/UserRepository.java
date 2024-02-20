28d1da256e92752c721165f718207a18b3f0fddc
Thiago Henrique <thiagohp070@gmail.com>
2024-02-19 21:31:46 -0300

    fix: remove unused method from user repository

--- backend-developer/microservices/user-service/src/main/java/com/thfp/userservice/entity/user/UserRepository.java
+++ backend-developer/microservices/user-service/src/main/java/com/thfp/userservice/entity/user/UserRepository.java
@@ -2,12 +2,9 @@ package com.thfp.userservice.entity.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    // jpa descriptive methods
-    // jpql
-    @Query("SELECT u FROM User u WHERE u.createdAt < :datetime")
-    User findAllByCreatedAtBefore(LocalDateTime dateTime);
 }
